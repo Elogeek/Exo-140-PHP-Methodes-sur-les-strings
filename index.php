f9770589e1df3469644d530c9e0a867ca85afcd1
Elogeek <elodiechristin@gmail.com>
2021-01-22 14:36:50 +0100

    start exo and finish

--- index.php
+++ index.php
@@ -1,23 +1,24 @@
 <?php
 
 // Compléter le code ci dessous pour afficher la longueur de la chaine de caracteres
+$text = "google";
+echo  strlen ($text);
 
-echo      ("google");
-
-//echo"<br><br>"; //Decommentez les <br> pour tester l'affichage lorsque vous aurez plusieurs instructions
+echo"<br><br>"; //Decommentez les <br> pour tester l'affichage lorsque vous aurez plusieurs instructions
 
 // Compléter le code ci dessous pour inverser la chaine de caractere
 
-echo  ("anticonstitutionellement");
+echo strrev ("anticonstitutionellement");
 
 // Et si vous testiez également avec votre prénom ?
 
+$name = "Eloggek";
+echo strrev($name);
 
-
-//echo"<br><br>";
+echo"<br><br>";
 
 // Compléter le code ci dessous pour remplacer le mot ok par non
 $oldtxt = "Ok Google!";
-$newtxt = (, , );
+$newtxt = str_replace("Ok","non",$oldtxt );
 
 echo $newtxt;
